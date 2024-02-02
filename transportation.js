06d8914dc6461c95052e7c744248461600ff97f6
mandyche <mandyche@umich.edu>
2024-02-02 17:19:00 -0500

    transportation

--- transportation.js
+++ transportation.js
@@ -1,18 +1,42 @@
 const currentMap = document.getElementById("currentMap");
 const currentSchedule = document.getElementById("currentSchedule");
+const currentScheduleDownload = document.getElementById("currentScheduleDownload");
+
 const bursleyBaits = document.getElementById("bursleyBaits");
 const commuterNorth = document.getElementById("commuterNorth");
 const commuterSouth = document.getElementById("commuterSouth");
+const crislerExpress = document.getElementById("crislerExpress");
+const diagToDiag = document.getElementById("diagToDiag");
+const glazierExpress = document.getElementById("glazierExpress");
+const medExpress = document.getElementById("medExpress");
+const northEastShuttle = document.getElementById("northEastShuttle");
+const northwood = document.getElementById("northwood");
+const northwoodExpress = document.getElementById("northwoodExpress");
+const oxfordShuttle = document.getElementById("oxfordShuttle");
+const wallStreet = document.getElementById("wallStreet");
 
-bursleyBaits.addEventListener("click", function() {
-    currentMap.src = "./transportation/map/bursleyBaits.png";
-    currentSchedule.src = "./transportation/schedule/bursleyBaitsS.png";
-});
-commuterNorth.addEventListener("click", function() {
-    currentMap.src = "./transportation/map/commuterNorth.png";
-    currentSchedule.src = "./transportation/schedule/commuterNorthS.png";
-});
-commuterSouth.addEventListener("click", function() {
-    currentMap.src = "./transportation/map/commuterSouth.png";
-    currentSchedule.src = "./transportation/schedule/commuterSouthS.png";
-});
+const locations = [bursleyBaits, commuterNorth, commuterSouth, crislerExpress, diagToDiag, glazierExpress,
+                    medExpress, northEastShuttle, northwood, northwoodExpress, oxfordShuttle, wallStreet];
+const locationsStrings = ["bursleyBaits", "commuterNorth", "commuterSouth", "crislerExpress", "diagToDiag", "glazierExpress",
+    "medExpress", "northEastShuttle", "northwood", "northwoodExpress", "oxfordShuttle", "wallStreet"];
+
+for (let i = 0; i < locations.length; i++) {
+    locations[i].addEventListener("click", function() {
+        currentMap.src = "./transportation/map/" + locationsStrings[i] + ".png";
+        currentSchedule.src = "./transportation/schedule/" + locationsStrings[i] +"S.png";
+        currentScheduleDownload.href = currentSchedule.src;
+    });
+}
+
+// bursleyBaits.addEventListener("click", function() {
+//     currentMap.src = "./transportation/map/bursleyBaits.png";
+//     currentSchedule.src = "./transportation/schedule/bursleyBaitsS.png";
+// });
+// commuterNorth.addEventListener("click", function() {
+//     currentMap.src = "./transportation/map/commuterNorth.png";
+//     currentSchedule.src = "./transportation/schedule/commuterNorthS.png";
+// });
+// commuterSouth.addEventListener("click", function() {
+//     currentMap.src = "./transportation/map/commuterSouth.png";
+//     currentSchedule.src = "./transportation/schedule/commuterSouthS.png";
+// });
